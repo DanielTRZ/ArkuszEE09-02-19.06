26dcfc764b102839e3239ba822b057187d74e7db
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-04-19 18:25:50 +0200

    Add files via upload

---
+++ oblicz.js
@@ -0,0 +1,28 @@
+
+      function oblicz(){
+          
+           var rodzajpaliwa1 = document.getElementById("rodzaj").value;
+           var wynik = document.getElementById("wynik");
+           var ilelitrow = document.getElementById("litr").value;
+           var tekst = "Koszt paliwa wynosi: ";
+          
+        
+          
+         if(rodzajpaliwa1 == 1){
+              
+              tekst += ilelitrow * 4 ;
+          }
+          else if(rodzajpaliwa1 == 2){
+              
+              tekst += ilelitrow * 3.5 ;
+          }
+          else{
+              tekst += " 0 ";
+          }
+           
+           wynik.innerHTML = tekst + "" + "zł";
+   
+          
+      
+}
+    
