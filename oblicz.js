f4ab65fcdb411227265b8de5dbd200cadf3cff8b
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2025-03-17 17:04:06 +0100

    Update oblicz.js

--- oblicz.js
+++ oblicz.js
@@ -4,7 +4,7 @@ function oblicz(){
 var rodzajpaliwa1 = document.getElementById("rodzaj").value;
 var wynik = document.getElementById("wynik");
 var ilelitrow = document.getElementById("litr").value;
-           var tekst = "Koszt paliwa wynosi: ";   
+          var tekst = "Koszt paliwa wynosi: ";   
           
          if(rodzajpaliwa1 == 1){
               
