b306b58f79b9f69246b9ade75b4c092b298f66e1
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2025-03-19 19:08:27 +0100

    Update oblicz.js

--- oblicz.js
+++ oblicz.js
@@ -12,7 +12,7 @@ tekst += ilelitrow * 4 ;
 }
 else if(rodzajpaliwa1 == 2){
               
-          tekst += ilelitrow * 3.5 ;
+tekst += ilelitrow * 3.5 ;
           }
           else{
               tekst += " 0 ";
